4b26a57b32b9e405d0bd046471c2f89b0d6bfe30
VitorVLima <vitor14_df@hotmail.com>
2025-03-16 13:15:05 -0300

    Alteracoes para publicacao no Heroku

--- microblog.py
+++ microblog.py
@@ -1,3 +1,14 @@
 from app import app
+import os
+
+if __name__ == 'main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
+
+
 
 #comando para iniciar flask --app microblog.py run --debug
+#instalar pip install flask
+#instalar pip install flask-dotenv
+#instalar pip install gunicorn - permite o uso do python como serviço web
+#comando pip freeze > requirements.txt para criar arquivo requirements
